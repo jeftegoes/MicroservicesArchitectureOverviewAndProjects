adde6fccaaed27a6d847a345ef192c50050a733d
Jefté Goes <jefte.goes.dev@outlook.com>
2023-08-01 23:45:46 -0300

    Containerizing Search.Api microservice.

--- ProjectAwsRecruitment/Search.Api/Program.cs
+++ ProjectAwsRecruitment/Search.Api/Program.cs
@@ -6,10 +6,10 @@ var app = builder.Build();
 
 app.MapGet("/search", async (string? city, int? rating) =>
 {
-    var host = "host";
-    var username = "userName";
-    var password = "password";
-    var indexName = "indexName";
+    var host = Environment.GetEnvironmentVariable("host");
+    var username = Environment.GetEnvironmentVariable("userName");
+    var password = Environment.GetEnvironmentVariable("password");
+    var indexName = Environment.GetEnvironmentVariable("indexName");
 
     var connSettings = new ConnectionSettings(new Uri(host));
     connSettings.BasicAuthentication(username, password);
@@ -17,6 +17,36 @@ app.MapGet("/search", async (string? city, int? rating) =>
     connSettings.DefaultMappingFor<Candidate>(n => n.IdProperty(p => p.Id));
 
     var esClient = new ElasticClient(connSettings);
+
+    if (rating is null)
+        rating = 1;
+
+    ISearchResponse<Candidate> result;
+
+    if (city is null)
+        result = await esClient.SearchAsync<Candidate>(
+            s => s.Query(
+                q => q.MatchAll()
+                &&
+                q.Range(
+                    p => p.Field(f => f.Rating).GreaterThanOrEquals(rating)
+                )
+            )
+        );
+
+    result = await esClient.SearchAsync<Candidate>(
+        s => s.Query(
+            q => q.Prefix(
+                p => p.Field(f => f.CityName).Value(city).CaseInsensitive()
+            )
+            &&
+            q.Range(
+                p => p.Field(f => f.Rating).GreaterThanOrEquals(rating)
+            )
+        )
+    );
+
+    return result.Hits.Select(x => x.Source).ToList();
 });
 
 app.Run();
---
+++ ProjectAwsRecruitment/Search.Api/README.md
@@ -0,0 +1,6 @@
+# Details
+
+- `Match` - where X = 'abc'
+- `Prefix` - where X like 'abc%'
+- `Range` - where between 0 and 10
+- `Fuzzy match` - where X like '%abc'
